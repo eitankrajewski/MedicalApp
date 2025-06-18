5edd60a0a915fa9809db63b5f03e6cecb681ccfd
Eitan Krajewski <eitankrajewski@yahoo.com>
2025-06-17 21:37:09 -0400

    Started UI

--- MedicalApp/Views/ContentView.swift
+++ MedicalApp/Views/ContentView.swift
@@ -12,10 +12,33 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            HStack {
+        VStack() {
+            HStack() {
+                VStack() {
+                    Text("Hello, [NAME]")
+                        .font(.system(size: 35, design: .serif))
+                    Text("How are you feeling?")
+                        .font(.system(.title3, design: .serif))
+                }
+                .frame(minWidth: 150, maxWidth: 500, alignment: .leading)
+//                .background(.red)
+                .padding(10)
+                
+                
+                Circle()
+                    .frame(width: 50)
                 
             }
+            .padding([.top], 50)
+            .padding([.leading, .trailing], 10)
+            
+            Rectangle()
+                .frame(width: 350, height: 200)
+                .cornerRadius(25)
+                .padding([.top], 15)
+                .shadow(color: .gray, radius: 5, x: 5, y: 5)
+            
+            Spacer()
         }
         
 //        Button("yuh") {
