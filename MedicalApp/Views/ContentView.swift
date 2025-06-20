5fd216238796c706a270c53c93f9029fe6e4b467
Eitan Krajewski <eitankrajewski@yahoo.com>
2025-06-20 10:27:35 -0400

    Homepage

--- MedicalApp/Views/ContentView.swift
+++ MedicalApp/Views/ContentView.swift
@@ -7,122 +7,155 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     @StateObject private var oauthFlow = OAuthFlow()
     
     var body: some View {
         
-        ScrollView {
-            VStack(spacing: 30) {
-                HStack() {
-                    VStack() {
-                        Text("Hello, [NAME]")
-                            .font(.system(size: 35, design: .serif))
-                        Text("How are you feeling?")
-                            .font(.system(.title3, design: .serif))
-                    }
-                    .frame(minWidth: 150, maxWidth: 500, alignment: .leading)
-                    //                .background(.red)
-                    .padding(10)
-                    
-                    
-                    Circle()
-                        .frame(width: 50)
-                    
-                }
-                .padding([.top], 50)
-                .padding([.leading, .trailing], 10)
-                
-                Rectangle()
-                    .frame(width: 350, height: 200)
-                    .cornerRadius(25)
-                    .padding([.top], 15)
-                    .shadow(color: .gray, radius: 5, x: 5, y: 5)
-                
-                HStack(spacing: 30) {
-                    Button {
+        TabView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    HStack() {
+                        VStack() {
+                            Text("Hello, [NAME]")
+                                .font(.system(size: 35, design: .serif))
+                            Text("How are you feeling?")
+                                .font(.system(.title3, design: .serif))
+                        }
+                        .frame(minWidth: 150, maxWidth: 500, alignment: .leading)
+                        //                .background(.red)
+                        .padding(10)
                         
-                    } label: {
-                        ZStack {
-
-                            VStack() {
-                                Image(systemName: "arrow.up.heart")
-                                    .font(.system(size: 50))
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, height: 50)
-                                
-                                Text("Report Symptoms")
-                                    .foregroundColor(.black)
-                                
-                            }
-                            .padding(6)
-                            .frame(width: 150, height: 150)
-                            .background(Color(.red))
-                            .cornerRadius(25)
+                        
+                        Circle()
+                            .frame(width: 50)
+                        
+                    }
+                    .padding([.top], 50)
+                    .padding([.leading, .trailing], 10)
+                    
+                    Rectangle()
+                        .frame(width: 350, height: 200)
+                        .cornerRadius(25)
+                        .padding([.top], 15)
+                        .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                    
+                    HStack(spacing: 30) {
+                        Button {
                             
-                            HStack() {
-                                Circle()
-                                    .frame(width: 50, height:50)
-                                    .overlay(
-                                        Text("+")
-                                            .foregroundColor(.black)
-                                            .font(.system(size: 45))
-                                            .offset(y:-3)
-                                    )
-                                    .offset(x: -55, y: -55)
+                        } label: {
+                            ZStack {
+                                
+                                VStack() {
+                                    Image(systemName: "arrow.up.heart")
+                                        .font(.system(size: 50))
+                                        .foregroundColor(.black)
+                                        .frame(width: 50, height: 50)
+                                    
+                                    Text("Report Symptoms")
+                                        .foregroundColor(.black)
+                                    
+                                }
+                                .padding(6)
+                                .frame(width: 150, height: 150)
+                                .background(Color(.red))
+                                .cornerRadius(25)
+                                
+                                HStack() {
+                                    Circle()
+                                        .frame(width: 50, height:50)
+                                        .overlay(
+                                            Text("+")
+                                                .foregroundColor(.black)
+                                                .font(.system(size: 45))
+                                                .offset(y:-3)
+                                        )
+                                        .offset(x: -55, y: -55)
+                                }
                             }
                         }
-                    }
-                    .shadow(radius: 10, x: 10, y: 10)
-                    
-                    Button {
+                        .shadow(radius: 10, x: 10, y: 10)
                         
-                    } label: {
-                        ZStack {
-
-                            VStack() {
-                                Image(systemName: "person.wave.2")
-                                    .font(.system(size: 50))
-                                    .foregroundColor(.black)
-                                    .frame(width: 50, height: 50)
-                                    .padding(5)
-                                
-                                Text("Report Health Data")
-                                    .foregroundColor(.black)
-                                
-                            }
-                            .padding(6)
-                            .frame(width: 150, height: 150)
-                            .background(Color(.red))
-                            .cornerRadius(25)
+                        Button {
                             
-                            HStack() {
-                                Circle()
-                                    .frame(width: 50, height:50)
-                                    .overlay(
-                                        Text("+")
-                                            .foregroundColor(.black)
-                                            .font(.system(size: 45))
-                                            .offset(y:-3)
-                                    )
-                                    .offset(x: -55, y: -55)
+                        } label: {
+                            ZStack {
+                                
+                                VStack() {
+                                    Image(systemName: "person.wave.2")
+                                        .font(.system(size: 50))
+                                        .foregroundColor(.black)
+                                        .frame(width: 50, height: 50)
+                                        .padding(5)
+                                    
+                                    Text("Report Health Data")
+                                        .foregroundColor(.black)
+                                    
+                                }
+                                .padding(6)
+                                .frame(width: 150, height: 150)
+                                .background(Color(.red))
+                                .cornerRadius(25)
+                                
+                                HStack() {
+                                    Circle()
+                                        .frame(width: 50, height:50)
+                                        .overlay(
+                                            Text("+")
+                                                .foregroundColor(.black)
+                                                .font(.system(size: 45))
+                                                .offset(y:-3)
+                                        )
+                                        .offset(x: -55, y: -55)
+                                }
                             }
                         }
+                        .shadow(radius: 10, x: 10, y: 10)
+                        
                     }
-                    .shadow(radius: 10, x: 10, y: 10)
-
+                    
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 350, height: 200)
+                            .cornerRadius(25)
+                            .padding([.top], 15)
+                            .shadow(color: .gray, radius: 5, x: 5, y: 5)
+                        
+                        Button() {
+                            
+                        } label: {
+                            Text("View Reports")
+                                .frame(maxWidth: 150)
+                                .frame(height: 25)
+                                .foregroundColor(.black)
+                                .background(.blue)
+                                .cornerRadius(25)
+                        }
+                        
+                        .offset(y: 70)
+                    }
+                    
+                    Spacer()
+                    
+                    
                 }
                 
-                Rectangle()
-                    .frame(width: 350, height: 200)
-                    .cornerRadius(25)
-                    .padding([.top], 15)
-                    .shadow(color: .gray, radius: 5, x: 5, y: 5)
                 
-                Spacer()
+            }
+            .toolbarBackground(.red, for: .tabBar)
+            .toolbarBackground(.visible, for: .tabBar)
+            .tabItem {
+                Image(systemName: "house")
+                Text("Hello")
+            }
+            
+            Text("Yes")
+            .tabItem {
+                Image(systemName: "arrow.up.heart")
+                Text("Upload Symptoms")
             }
         }
-        
 //        Button("yuh") {
 //            oauthFlow.startOAuthFlow()
 //        }
---
+++ MedicalApp/Views/SymptomView.swift
@@ -0,0 +1,18 @@
+//
+//  SymptomView.swift
+//  MedicalApp
+//
+//  Created by Eitan Krajewski on 6/20/25.
+//
+
+import SwiftUI
+
+struct SymptomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SymptomView()
+}
